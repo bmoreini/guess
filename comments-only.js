63bae07e827835d4f7a8abf9ea4fa4222cb75fda
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-09-08 21:44:45 -0400

    Create comments-only.js

---
+++ comments-only.js
@@ -0,0 +1,30 @@
+// initialize Answer: generate random number btween 0 and 1, multiply by 1, convert to integer with round up
+
+// initialize Turns variable, set to 1
+
+// initialize Guess variable, set to 0
+
+// initialize boolean variable Correct, set to value False
+
+// Keep looping while Correct is false
+
+    // prompt user for Guess, tell user what to input
+
+    // check if guess is equal to answer
+
+        // if equal, say "Correct" and number of turns
+ 
+        // set Correct equal to true to get out of loop
+
+
+    // check if guess greater than answer
+
+        // give feedback: go lower
+
+    // check if guess lower than answer
+
+        // give feedback: go higher
+
+    // add one to turns
+
+// thank player for playing
