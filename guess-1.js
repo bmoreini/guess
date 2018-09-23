75f7661b8b92a3e24dcf6a286977ad3cc69a8d2d
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-09-22 20:13:52 -0400

    Update guess-1.js

--- guess-1.js
+++ guess-1.js
@@ -1,18 +1,12 @@
-var turns=1;
+var turns=0;
 var correctAnswer=false;
 var answer = Math.round(Math.random()*100)+1;
 console.log(answer);
 while (correctAnswer==false) {
     var guess = prompt("Guess my integer (1-100)");
-    if (guess == answer) {
-        correctAnswer = true;
-    }
-    if (guess > answer) {
-        alert("Try a lower number!");
-    }
-    else if (guess < answer) {
-        alert("Try a higher number!");
-    }
+    if (guess == answer) correctAnswer = true;
+    if (guess > answer) alert("Try a lower number!");
+    if (guess < answer) alert("Try a higher number!");
     turns++;
 }
 alert("Congratulations! You guessed it in "+turns+" turns.");
